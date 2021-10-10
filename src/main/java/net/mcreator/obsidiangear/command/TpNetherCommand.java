a1fbb289a48da36f59e301f6484f0e85f0d82a0f
SniperNation <snipernationsim@gmail.com>
2021-10-10 19:24:10 -0400

    Added commands to teleport to dimensions

---
+++ src/main/java/net/mcreator/obsidiangear/command/TpNetherCommand.java
@@ -0,0 +1,56 @@
+
+package net.mcreator.obsidiangear.command;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.common.util.FakePlayerFactory;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.command.Commands;
+import net.minecraft.command.CommandSource;
+
+import net.mcreator.obsidiangear.procedures.TpNetherCommandExecutedProcedure;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
+@Mod.EventBusSubscriber
+public class TpNetherCommand {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent event) {
+		event.getDispatcher()
+				.register(LiteralArgumentBuilder.<CommandSource>literal("tpnether").requires(s -> s.hasPermissionLevel(4))
+						.then(Commands.argument("arguments", StringArgumentType.greedyString()).executes(TpNetherCommand::execute))
+						.executes(TpNetherCommand::execute));
+	}
+
+	private static int execute(CommandContext<CommandSource> ctx) {
+		ServerWorld world = ctx.getSource().getWorld();
+		double x = ctx.getSource().getPos().getX();
+		double y = ctx.getSource().getPos().getY();
+		double z = ctx.getSource().getPos().getZ();
+		Entity entity = ctx.getSource().getEntity();
+		if (entity == null)
+			entity = FakePlayerFactory.getMinecraft(world);
+		HashMap<String, String> cmdparams = new HashMap<>();
+		int[] index = {-1};
+		Arrays.stream(ctx.getInput().split("\\s+")).forEach(param -> {
+			if (index[0] >= 0)
+				cmdparams.put(Integer.toString(index[0]), param);
+			index[0]++;
+		});
+		{
+			Map<String, Object> $_dependencies = new HashMap<>();
+			$_dependencies.put("entity", entity);
+			TpNetherCommandExecutedProcedure.executeProcedure($_dependencies);
+		}
+		return 0;
+	}
+}
---
+++ src/main/java/net/mcreator/obsidiangear/procedures/TpOverworldCommandExecutedProcedure.java
@@ -0,0 +1,45 @@
+package net.mcreator.obsidiangear.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.network.play.server.SPlayerAbilitiesPacket;
+import net.minecraft.network.play.server.SPlaySoundEventPacket;
+import net.minecraft.network.play.server.SPlayEntityEffectPacket;
+import net.minecraft.network.play.server.SChangeGameStatePacket;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.obsidiangear.ObsidianGearMod;
+
+import java.util.Map;
+
+public class TpOverworldCommandExecutedProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				ObsidianGearMod.LOGGER.warn("Failed to load dependency entity for procedure TpOverworldCommandExecuted!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		{
+			Entity _ent = entity;
+			if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+				RegistryKey<World> destinationType = World.OVERWORLD;
+				ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+				if (nextWorld != null) {
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+					((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), nextWorld.getSpawnPoint().getY() + 1,
+							nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+					for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+						((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+					}
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SPlaySoundEventPacket(1032, BlockPos.ZERO, 0, false));
+				}
+			}
+		}
+	}
+}
