c0f0a3c6ef1a1848bdd7b1cc283ce10fa9c90225
SniperNation <snipernationsim@gmail.com>
2021-12-13 18:12:06 -0500

    Added wood type, created new biome and changed tags
    New wood type: Spook Wood
    New Biome: Spook Forest
    Added minecraft:logs tag to Spook Log

---
+++ src/main/java/net/mcreator/obsidiangear/block/SpookLeavesBlock.java
@@ -0,0 +1,62 @@
+
+package net.mcreator.obsidiangear.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.obsidiangear.ObsidianGearModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@ObsidianGearModElements.ModElement.Tag
+public class SpookLeavesBlock extends ObsidianGearModElements.ModElement {
+	@ObjectHolder("obsidian_gear:spook_leaves")
+	public static final Block block = null;
+	public SpookLeavesBlock(ObsidianGearModElements instance) {
+		super(instance, 132);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends LeavesBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(0.2f, 0.2f).setLightLevel(s -> 0).notSolid());
+			setRegistryName("spook_leaves");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 1;
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 30;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
