b8a7b06df2d97cf4f93e31cd03a226258ebce74d
SniperNation <snipernationsim@gmail.com>
2021-12-14 17:22:10 -0500

    Created a painting for Spook Forest
    It will randomly appear when placing paintings

---
+++ src/main/java/net/mcreator/obsidiangear/painting/SpookForestPaintingPainting.java
@@ -0,0 +1,16 @@
+
+package net.mcreator.obsidiangear.painting;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.entity.item.PaintingType;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SpookForestPaintingPainting {
+	@SubscribeEvent
+	public static void registerPaintingType(RegistryEvent.Register<PaintingType> event) {
+		event.getRegistry().register(new PaintingType(48, 32).setRegistryName("spook_forest_painting"));
+	}
+}
