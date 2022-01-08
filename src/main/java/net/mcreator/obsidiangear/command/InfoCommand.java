af27e063dbb60202ece228f9200ec2ab8aa81f6c
SniperNation <snipernationsim@gmail.com>
2022-01-08 13:52:00 -0500

    Added new cmd: /info and updated /help command

---
+++ src/main/java/net/mcreator/obsidiangear/command/InfoCommand.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.obsidiangear.command;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.common.util.FakePlayerFactory;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.CommandSourceStack;
+
+import net.mcreator.obsidiangear.procedures.InfoCommandExecutedProcedure;
+
+import java.util.HashMap;
+import java.util.Arrays;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
+@Mod.EventBusSubscriber
+public class InfoCommand {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent event) {
+		event.getDispatcher().register(Commands.literal("info")
+
+				.then(Commands.argument("arguments", StringArgumentType.greedyString()).executes(InfoCommand::execute))
+				.executes(InfoCommand::execute));
+	}
+
+	private static int execute(CommandContext<CommandSourceStack> ctx) {
+		ServerLevel world = ctx.getSource().getLevel();
+		double x = ctx.getSource().getPosition().x();
+		double y = ctx.getSource().getPosition().y();
+		double z = ctx.getSource().getPosition().z();
+		Entity entity = ctx.getSource().getEntity();
+		if (entity == null)
+			entity = FakePlayerFactory.getMinecraft(world);
+		HashMap<String, String> cmdparams = new HashMap<>();
+		int[] index = {-1};
+		Arrays.stream(ctx.getInput().split("\\s+")).forEach(param -> {
+			if (index[0] >= 0)
+				cmdparams.put(Integer.toString(index[0]), param);
+			index[0]++;
+		});
+
+		InfoCommandExecutedProcedure.execute(entity);
+		return 0;
+	}
+}
--- src/main/java/net/mcreator/obsidiangear/procedures/InfoCommandExecutedProcedure.java
+++ src/main/java/net/mcreator/obsidiangear/procedures/InfoCommandExecutedProcedure.java
@@ -4,17 +4,19 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.TextComponent;
 
-public class HelpCmdCommandExecutedProcedure {
+public class InfoCommandExecutedProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof Player _player && !_player.level.isClientSide())
-			_player.displayClientMessage(new TextComponent("Visit https://github.com/SniperNation/obsidian-gear-mod/ for help..."), (false));
+			_player.displayClientMessage(new TextComponent("Obsidian Gear Mod"), (false));
 		if (entity instanceof Player _player && !_player.level.isClientSide())
-			_player.displayClientMessage(new TextComponent("Commands:"), (false));
+			_player.displayClientMessage(new TextComponent("(C) 2021 - 2022 Aashay Ghodgaonkar"), (false));
 		if (entity instanceof Player _player && !_player.level.isClientSide())
-			_player.displayClientMessage(new TextComponent("/tp<dimension> - Teleports player to specified dimension"), (false));
+			_player.displayClientMessage(new TextComponent(
+					"Adds a buncha new stuff to MC! This includes new biomes, dimensions, blocks, items, weapons, armor, recipes and so much more!"),
+					(false));
 		if (entity instanceof Player _player && !_player.level.isClientSide())
-			_player.displayClientMessage(new TextComponent("/info - Gives info about the mod"), (false));
+			_player.displayClientMessage(new TextComponent("Website: https://github.com/SniperNation/obsidian-gear-mod"), (false));
 	}
 }
